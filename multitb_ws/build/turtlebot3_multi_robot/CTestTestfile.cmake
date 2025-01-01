41ba2dc73e1a7398c71e9425a9f7d06aa635b056
viator <pedralation@gmail.com>
2025-01-01 17:29:50 +0900

    add walleva

--- multitb_ws/build/turtlebot3_multi_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/viator/ws/walleva_ws/multitb_ws/src/turtlebot3_multi_robot
-# Build directory: /home/viator/ws/walleva_ws/multitb_ws/build/turtlebot3_multi_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ multitb_ws/build/turtlebot3_walleva/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/viator/ws/walleva_ws/multitb_ws/src/turtlebot3_walleva
+# Build directory: /home/viator/ws/walleva_ws/multitb_ws/build/turtlebot3_walleva
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
